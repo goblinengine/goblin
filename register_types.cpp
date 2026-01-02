0ff4759cb4d6a08428966730f7b98f94980fcf94
filipworks <filipworksdev@users.noreply.github.com>
2026-01-02 14:01:21 +0100

    goblin changes to editor should only be allowed when tools are enabled

--- register_types.cpp
+++ register_types.cpp
@@ -7,11 +7,14 @@
 
 #include "register_types.h"
 
-#include "editor/goblin_about.h"
 #include "core/config/engine.h"
 #include "core/object/class_db.h"
 
+#ifdef TOOLS_ENABLED
+#include "editor/goblin_about.h"
+
 static GoblinBranding *goblin_branding = nullptr;
+#endif
 
 void preregister_goblin_types() {
 	// Called before other modules for docgen
@@ -20,6 +23,7 @@ void preregister_goblin_types() {
 void initialize_goblin_module(ModuleInitializationLevel p_level) {
 	// Project Manager + Editor UI are initialized at EDITOR level.
 	// We also initialize at SCENE level to cover non-editor runtime usage.
+#ifdef TOOLS_ENABLED
 	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR || p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
 		// Register Goblin branding class
 		GDREGISTER_CLASS(GoblinBranding);
@@ -30,13 +34,16 @@ void initialize_goblin_module(ModuleInitializationLevel p_level) {
 			goblin_branding->initialize();
 		}
 	}
+#endif
 }
 
 void uninitialize_goblin_module(ModuleInitializationLevel p_level) {
+#ifdef TOOLS_ENABLED
 	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR || p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
 		if (goblin_branding) {
 			memdelete(goblin_branding);
 			goblin_branding = nullptr;
 		}
 	}
+#endif
 }
